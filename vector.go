7945876c02e7ce6ac2aa67dac5523066bea707a2
Michael Clifford <cliffom@gmail.com>
2023-07-25 14:53:28 -0700

    Added more vector methods and tests

--- vector.go
+++ vector.go
@@ -29,3 +29,50 @@ func (v Vector3D) Normalize() (Vector3D, error) {
 		Z: v.Z / mag,
 	}, nil
 }
+
+// Add adds two vectors together
+func (v Vector3D) Add(w Vector3D) Vector3D {
+	return Vector3D{
+		X: v.X + w.X,
+		Y: v.Y + w.Y,
+		Z: v.Z + w.Z,
+	}
+}
+
+// Subtract subtracts one vector from another
+func (v Vector3D) Subtract(w Vector3D) Vector3D {
+	return Vector3D{
+		X: v.X - w.X,
+		Y: v.Y - w.Y,
+		Z: v.Z - w.Z,
+	}
+}
+
+// Multiply scales a vector by a scalar
+func (v Vector3D) Multiply(scalar float64) Vector3D {
+	return Vector3D{
+		X: v.X * scalar,
+		Y: v.Y * scalar,
+		Z: v.Z * scalar,
+	}
+}
+
+// DotProduct calculates the dot product of two vectors
+func (v Vector3D) DotProduct(w Vector3D) float64 {
+	return v.X*w.X + v.Y*w.Y + v.Z*w.Z
+}
+
+// CrossProduct calculates the cross product of two vectors
+func (v Vector3D) CrossProduct(w Vector3D) Vector3D {
+	return Vector3D{
+		X: v.Y*w.Z - v.Z*w.Y,
+		Y: v.Z*w.X - v.X*w.Z,
+		Z: v.X*w.Y - v.Y*w.X,
+	}
+}
+
+// Distance calculates the distance between two vectors
+func (v Vector3D) Distance(w Vector3D) float64 {
+	difference := v.Subtract(w)
+	return difference.Magnitude()
+}
